61964d6397093ef077672f09b8daf68d2ef63b23
Gregg Carey <gregg.carey@outlook.com>
2024-04-12 10:49:28 -0400

    top level functions

---
+++ Chapter01/TopLevelFunctions/Program.Functions.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+partial class Program
+{
+    static void WhatsMyNamespace()//This is a static function
+    {
+        WriteLine($"Namespace of the Program class: {typeof(Program).Namespace ?? "null"}");
+    }
+}
