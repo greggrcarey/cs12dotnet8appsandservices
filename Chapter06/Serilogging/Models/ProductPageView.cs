ffeaa2adba47e03d7f8038d004bb6f599746363e
Gregg Carey <gregg.carey@outlook.com>
2024-06-04 11:47:57 -0400

    AutoMapper Humaizer Serilog

---
+++ Chapter06/Serilogging/Models/ProductPageView.cs
@@ -0,0 +1,7 @@
+﻿namespace Serilogging.Models;
+public class ProductPageView
+{
+    public int ProductId { get; set; }
+    public string? PageTitle { get; set; }
+    public string? SiteSection { get; set; }
+}
