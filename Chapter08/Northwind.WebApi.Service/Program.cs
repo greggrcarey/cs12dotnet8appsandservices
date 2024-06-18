2f1d18e8dbbf04fd10244df77bc3942cd735e256
Gregg Carey <gregg.carey@outlook.com>
2024-06-18 14:23:46 -0400

    asp.net

--- Chapter08/Northwind.WebApi.Service/Program.cs
+++ Chapter08/Northwind.WebApi.Service/Program.cs
@@ -1,9 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Northwind.EntityModels; // To use the AddNorthwindContext method.
+using Packt.Extensions; // To use MapGets and so on.
 
+
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddNorthwindContext();
+builder.Services.AddCustomHttpLogging();
+
 
 var app = builder.Build();
 
@@ -13,32 +20,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseHttpLogging();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
+app.MapGets() // Default pageSize: 10.
+  .MapPosts()
+  .MapPuts()
+  .MapDeletes();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+//Creating a web page JavaScript client chapter 8
