dce7da15b178d2c37147aa13c907b857c447f187
Gregg Carey <gregg.carey@outlook.com>
2024-07-05 10:26:51 -0400

    timer worker example

--- Chapter09/Northwind.Background.Workers/Program.cs
+++ Chapter09/Northwind.Background.Workers/Program.cs
@@ -2,6 +2,7 @@ using Northwind.Background.Workers;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<QueueWorker>();
+builder.Services.AddHostedService<TimerWorker>();
 
 var host = builder.Build();
 host.Run();
