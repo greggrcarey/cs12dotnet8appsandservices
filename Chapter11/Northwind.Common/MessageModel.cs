d537de86bda6407c478a774f59784accb8a0e252
Gregg Carey <gregg.carey@outlook.com>
2024-07-11 15:34:48 -0400

    Example SignalR Implementation

---
+++ Chapter11/Northwind.Common/MessageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Northwind.Chat.Models;
+public class MessageModel
+{
+    public string From { get; set; } = null!;
+    public string To { get; set; } = null!;
+    public string? Body { get; set; }
+}
