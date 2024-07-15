36d9ae743e565a8c69fd9bf1535a4df29f5db633
Gregg Carey <gregg.carey@outlook.com>
2024-07-15 10:21:57 -0400

    basic gql queries

--- Chapter12/Northwind.GraphQL.Service/Program.cs
+++ Chapter12/Northwind.GraphQL.Service/Program.cs
@@ -1,6 +1,14 @@
+using Northwind.GraphQL.Service;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Navigate to https://localhost:5121/grapql");
+app.MapGraphQL();
 
 app.Run();
---
+++ Chapter12/Northwind.GraphQL.Service/Query.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.GraphQL.Service;
+
+public class Query
+{
+    public string GetGreeting() => "Hello, World!";
+    public string GetFarewell() => "Ciao! Ciao!";
+    public int RollTheDie() => Random.Shared.Next(1, 7);
+}
