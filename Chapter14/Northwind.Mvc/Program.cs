5b7d5fe06fd4dd35bee7cf21f95efe4d8a6764dc
Gregg Carey <gregg.carey@outlook.com>
2024-07-22 10:14:29 -0400

    Translation example

--- Chapter14/Northwind.Mvc/Program.cs
+++ Chapter14/Northwind.Mvc/Program.cs
@@ -16,7 +16,11 @@ builder.Services.AddNorthwindContext();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews().AddViewLocalization();
+
 
 var app = builder.Build();
 
@@ -31,6 +35,16 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+string[] cultures = { "en-US", "en-GB", "fr-FR", "fr" };
+RequestLocalizationOptions localizationOptions = new();
+localizationOptions.SetDefaultCulture(cultures[0]) //en-US
+    //globalization of data formats like dates and currencies
+    .AddSupportedCultures(cultures)
+    //Set localization of user interface text
+    .AddSupportedUICultures(cultures);
+app.UseRequestLocalization(localizationOptions);
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
