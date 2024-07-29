9d79e5e522873271c0c6058aaac8f7277467c522
Gregg Carey <gregg.carey@outlook.com>
2024-07-29 12:01:56 -0400

    different syntax

--- Chapter16/Northwind.Maui.Client/EmployeesPage.xaml.cs
+++ Chapter16/Northwind.Maui.Client/EmployeesPage.xaml.cs
@@ -1,27 +1,32 @@
+using System.Runtime.Versioning;
+
 namespace Northwind.Maui.Client;
 
 public partial class EmployeesPage : ContentPage
 {
-	public EmployeesPage()
-	{
-		InitializeComponent();
-	}
-	private void ContentPage_Loaded(object sender, EventArgs e)
-	{
-		foreach(Button button in GridCalculator.Children.OfType<Button>())
-		{
-			button.FontSize = 24;
-			button.WidthRequest = 54;
-			button.HeightRequest = 54;
-			button.Clicked += Button_Clicked;
-		}
-	}
-
-    private void Button_Clicked(object sender, EventArgs e)
+    public EmployeesPage()
     {
-		string operationChars = "+-/X=";
-		Button button = (Button)sender;
+        InitializeComponent();
+    }
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        foreach (Button button in GridCalculator.Children.OfType<Button>())
+        {
+            button.FontSize = 24;
+            button.WidthRequest = 54;
+            button.HeightRequest = 54;
+            button.Clicked += Button_Clicked;
+        }
+    }
 
+    private void Button_Clicked(object? sender, EventArgs e)
+    {
+        if (sender is not Button button)
+        {
+            return;
+        }
+
+        string operationChars = "+-/X=";
         _ = operationChars.Contains(button.Text) ? Output.Text = string.Empty : Output.Text += button.Text;
 
     }
