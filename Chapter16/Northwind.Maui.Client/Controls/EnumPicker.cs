99193bb0b92408b60163ed80522830245d8d2b31
Gregg Carey <gregg.carey@outlook.com>
2024-07-29 16:28:52 -0400

    Theme selector. Mostly works

---
+++ Chapter16/Northwind.Maui.Client/Controls/EnumPicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Northwind.Maui.Client.Controls;
+
+internal class EnumPicker : Picker
+{
+    public Type EnumType
+    {
+        get => (Type)GetValue(EnumTypeProperty);
+        set => SetValue(EnumTypeProperty, value);
+    }
+
+    public static readonly BindableProperty EnumTypeProperty =
+        BindableProperty.Create(
+            propertyName: nameof(EnumType),
+            returnType: typeof(Type),
+            declaringType: typeof(EnumPicker),
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                EnumPicker picker = (EnumPicker)bindable;
+                if (oldValue is not null)
+                {
+                    picker.ItemsSource = null;
+                }
+                if (newValue is not null)
+                {
+                    if (!((Type)newValue).GetTypeInfo().IsEnum)
+                        throw new ArgumentException(
+                            "EnumPicker: EnumType property must be enumeration type");
+                    picker.ItemsSource = Enum.GetValues((Type)newValue);
+                }
+            });
+
+}
--- Chapter16/Northwind.Maui.Client/SettingsPage.xaml.cs
+++ Chapter16/Northwind.Maui.Client/SettingsPage.xaml.cs
@@ -1,3 +1,5 @@
+using Northwind.Maui.Client.Controls; //To use enum Theme.
+using Northwind.Maui.Client.Resources.Styles; //To use DarkModeTheme
 namespace Northwind.Maui.Client;
 
 public partial class SettingsPage : ContentPage
@@ -6,4 +8,35 @@ public partial class SettingsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ThemePicker_SelectionChanged(object sender, EventArgs e)
+    {
+        if (sender is not Picker picker) return;
+
+        Theme theme = (Theme)picker.SelectedItem;
+
+        if (Application.Current is null) return;
+
+        ICollection<ResourceDictionary> resources = Application.Current.Resources.MergedDictionaries;
+
+        if(resources is not null)
+        {
+            resources.Clear();
+            resources.Add(new Resources.Styles.Northwind());
+            resources.Add(new LightDarkModeColors());
+            ResourceDictionary themeResource = theme switch
+            {
+                Theme.Dark => new DarkModeTheme(),
+                Theme.Light => new LightModeTheme(),
+                _ => new SystemModeTheme()
+            };
+            resources.Add(themeResource);
+        }
+
+    }
+
+    private void ThemePicker_Loaded(object sender, EventArgs e)
+    {
+        ThemePicker.SelectedItem = Theme.System;
+    }
 }
