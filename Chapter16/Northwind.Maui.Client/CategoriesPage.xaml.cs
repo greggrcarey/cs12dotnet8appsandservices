ad7a0c49032f8ddb32e20419b7151df75fd5dda4
Gregg Carey <gregg.carey@outlook.com>
2024-07-29 10:43:18 -0400

    Maui example

---
+++ Chapter16/Northwind.Maui.Client/CategoriesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Northwind.Maui.Client;
+
+public partial class CategoriesPage : ContentPage
+{
+	public CategoriesPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chapter16/Northwind.Maui.Client/EmployeesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Northwind.Maui.Client;
+
+public partial class EmployeesPage : ContentPage
+{
+	public EmployeesPage()
+	{
+		InitializeComponent();
+	}
+}
